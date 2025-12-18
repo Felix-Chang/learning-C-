a1fd9405ab493787352a2a779a17ea5d90ef23e8
Felix Chang <felixchang@Felixs-MacBook-Air.local>
2025-12-18 17:48:52 -0600

    message idk

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+
+    // int file_size = 100;
+    // int counter = 0;
+    // double sales = 9.99;
+    // std::cout << file_size;
+
+    // int a = 1;
+    // int b = 2;
+    // int temp = a;
+
+    // a = b;
+    // b = temp;
+    // std::cout << a;
+
+
+    
+    return 0;
+}
+
+
