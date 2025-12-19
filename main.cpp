58e9cbe1739d38897a748f3fd9a347ece8ce82c2
Felix Chang <felixchang@Felixs-MacBook-Air.local>
2025-12-18 20:00:17 -0600

    std and data types

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
 
     // int file_size = 100;
@@ -15,9 +17,39 @@ int main() {
     // b = temp;
     // std::cout << a;
 
+    // const double pi = 3.14;
+
+    // double x = 10;
+    // int y = 3;
+    // double z = x / y;
+
+    // std::cout << z;
+
+    // int x = 10;
+    // int y = x++; //. x = 11, y = 10 --> first assign, then increment. suffix
+    // int z = ++x; //. x = 11, z = 11 --> first increment, then assign. prefix
+
+
+    // double x = 1 + 2 * 3; // order of operations
+
+    // int x = 10;
+    // int y = 20;
+    // // std::cout << "x = ";
+    // // std::cout << x;
+    
+    // cout << "x = " << x << endl
+    //      << "y = " << y; 
+    
+    
 
     
-    return 0;
-}
 
+
+
+
+
+
+    return 0;
+
+}
 
