99b10a4a2b4383e8e88fa9d67b8e25d359337720
Felix Chang <felixchang@Felixs-MacBook-Air.local>
2025-12-24 17:00:02 -0600

    operators

--- tutorial2.cpp
+++ tutorial2.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace first {
+    int x = 1;
+}
+
+namespace second {
+    int x = 2;
+}
+
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+// typedef std::string text_t;
+
+using text_t = std::string; // alternate, better way of creating new type 
 
 int main() {
+    string name = "Bro";
+    int x = 0;
+
+
+    // using namespace first;
+
+    // cout << x << endl;
+    // cout << first::x << endl;
+    // cout << second::x << endl;
+
+    // pairlist_t pairlist;
+
+
+    // text_t firstname = "bro";
+
+    // int student = 2;
+
+    // student++;
+
+    int x = 3.14;
+
+    cout << x;
 
 
 
@@ -12,10 +50,5 @@ int main() {
 
 
 
-
-
-
-
-    
     return 0;
 }
